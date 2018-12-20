9e6aa1bae3a87e33fbcf3467f29d18ea7075459d
Jordi Andreu <jandreu@cells.es>
2018-12-20 15:30:21 +0100

    Improve script for deployment

--- utils/deploy_from_git.sh
+++ utils/deploy_from_git.sh
@@ -1,30 +1,58 @@
 #!/usr/bin/env bash
-
+##############################################################################
+#
+# Author: Jordi Andreu
+# Last revision: 20 Dec 2018
+#
 # Create a tar archive that contains the contents of the latest commit
 # on the current branch.
+#
+##############################################################################
 
+# Check that the first argument is not an empty string and it is a valid
+# existing directory.
+if ([[ -z "$1" ]] || [[ ! -d $1 ]])
+  then
+    echo "You MUST supply a valid directory to deploy the solution. Exiting!."
+    exit -1
+fi
+
+# Set the deployment folder
+DEPLOYDIR=$1
+
+# Check we are in a git repository.
 ROOT=$(git rev-parse --show-toplevel)
-echo $ROOT
 
 if [[ $ROOT == *"fatal"* ]]; then
-  echo "No repository!"
+  echo "We are not into a valid git repository. Existing!"
   exit -1
 fi
 
-NAME=$(basename $ROOT)
-echo $NAME
+# Get the repository folder root name.
+REPONAME=$(basename $ROOT)
 
+# Get the hash of the latest commit.
 HASH=$(git log --pretty=format:'%h' -n 1)
 
-ZIPFILE=$NAME-$HASH.tar.gz
+# Get current date.
+DATE=`date '+%Y%m%d_%H%M%S'`
+#DATE=`date '+%Y%m%d'`
 
-git archive --format=tar --prefix=$NAME/ HEAD | gzip >$ZIPFILE
+NAME=$REPONAME-$DATE-$HASH
+TARGZFILE=$NAME.tar.gz
 
-# Extract the snapshot to production folder
-PRODDIR=$1
+# Archive the repository to a tar-gz file
+echo "Archiving repository $REPONAME as $TARGZFILE"
+git archive --format=tar --prefix=$NAME/ HEAD | gzip >$TARGZFILE
 
-mv $ZIPFILE $PRODDIR
-pushd $PRODDIR
-    tar -xf $ZIPFILE
-popd
-
+# Extract the archived code to production folder
+echo "Deploying repository $REPONAME to $DEPLOYDIR"
+mv $TARGZFILE $DEPLOYDIR
+pushd $DEPLOYDIR > /dev/null
+    tar -xf $TARGZFILE
+    if [[ -L $REPONAME ]]; then
+        rm -r $REPONAME
+    fi
+    ln -s $NAME $REPONAME
+popd > /dev/null
+echo "Done."
--- utils/deploy_mxcube.sh
+++ utils/deploy_mxcube.sh
@@ -1,36 +1,31 @@
 #!/usr/bin/env bash
+##############################################################################
+#
+# Author: Jordi Andreu
+# Last revision: 20 Dec 2018
+#
+# Deploy MXCuBE project from git repositories
+#
+##############################################################################
 
-PRODDIR=/homelocal/sicilia/production
+# Log into the mxcube root repository
+GITDIR=$HOME/git
+DEPLOYER=$HOME/staging/MXCuBE-devel/bl13_config/utils/deploy_from_git.sh
+MXCUBE_REPO=$GITDIR/mxcube
+HWREPO_REPO=$GITDIR/mxcube/HardwareRepository
+CONFIG_REPO=$GITDIR/bl13_mxcube_config
+DEST=$HOME/production
 
-ROOT=$(git rev-parse --show-toplevel)
-echo $ROOT
+# Start deploying the mxcube project (mxcube + hwrepo + bl13_config)
+for REPO in $MXCUBE_REPO $HWREPO_REPO $CONFIG_REPO
+do
+  pushd $REPO > /dev/null
+    $DEPLOYER $DEST
+  popd > /dev/null
+done
 
-if [[ $ROOT == *"fatal"* ]]; then
-  echo "No repository!"
-  exit -1
-fi
-
-NAME=$(basename $ROOT)
-echo $NAME
-
-if [[ $NAME != "mxcube" ]]; then
-  echo "No mxcube repo"
-  exit -1
-fi
-
-DEPLOY="/homelocal/sicilia/staging/MXCuBE-devel/bl13_config/utils/deploy_from_git.sh"
-
-$DEPLOY $PRODDIR
-
-HWREPDIR=$PRODDIR/"mxcube"
-
-pushd "HardwareRepository"
-
-  $DEPLOY $HWREPDIR
-
-popd
-
-pushd ../bl13_mxcube_config
-
-$DEPLOY $PRODDIR
-
+# Create a link from mxcube/HardwareRepository to HardwareRepository
+pushd $DEST/mxcube > /dev/null
+  rm -r "HardwareRepository"
+  ln -s $DEST/HardwareRepository
+popd > /dev/null
